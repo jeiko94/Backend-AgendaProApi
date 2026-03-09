dc9c82dd7bcd408a6f9c8646b027abed0f03f2c9
omar peña <omarpk148@gmail.com>
2026-03-09 18:12:26 -0500

    CORS (para React)

--- Backend-AgendaProApi.Api/Program.cs
+++ Backend-AgendaProApi.Api/Program.cs
@@ -14,6 +14,24 @@ builder.Services.AddDbContext<AgendaDbContext>(options =>
 builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddControllers();
+
+// CORS (para React)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact",
+        policy =>
+        {
+            policy.WithOrigins(
+                "http://localhost:5173",
+                "http://localhost:3000",
+                "http://localhost:8080"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -26,6 +44,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+// Activar CORS
+app.UseCors("AllowReact");
 
 app.UseHttpsRedirection();
 
