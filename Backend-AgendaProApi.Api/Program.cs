1c22d6e33044bfac13a4b5aae3536dee2370daa1
Diego Muñoz <alejo31mzz@gmail.com>
2026-03-09 20:52:02 -0500

    prueba3 ci/cd

--- Backend-AgendaProApi.Api/Program.cs
+++ Backend-AgendaProApi.Api/Program.cs
@@ -24,35 +24,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "LorianAnalytics API",
-        Version = "v1",
-        Description = "API para LorianAnalytics con autenticación JWT"
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-var swaggerEnabled = builder.Configuration.GetValue<bool>("Swagger:Enabled");
+var enableSwagger = builder.Configuration.GetValue<bool>("EnableSwagger");
 
-if (swaggerEnabled || app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || enableSwagger)
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "LorianAnalytics API v1");
-        c.DisplayRequestDuration();
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
